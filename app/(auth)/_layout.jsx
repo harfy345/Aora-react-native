ea28b2c6891e4fd3dcca7393a789969653d89682
harfy345 <hakam.almotlk345@gmail.com>
2024-04-14 14:59:42 -0400

    init

---
+++ app/(auth)/_layout.jsx
@@ -0,0 +1,29 @@
+import { View, Text } from 'react-native'
+import {StatusBar} from 'expo-status-bar'
+import {Stack} from 'expo-router'
+import React from 'react'
+
+const AuthLayout = () => {
+  return (
+    <>
+    <Stack>
+      <Stack.Screen 
+        name="sign-in"
+        options={{
+          headerShown:false,
+        }}
+      />
+      
+       <Stack.Screen 
+        name="sign-up"
+        options={{
+          headerShown:false,
+        }}
+      />
+    </Stack>
+    <StatusBar style='light'/>
+    </>
+  )
+}
+
+export default AuthLayout
